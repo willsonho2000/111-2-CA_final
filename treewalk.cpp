fe03971fd8a94fd57a327065e530f6868e059f22
Anchewei <10302leo@gmail.com>
2023-05-25 13:51:56 +0800

    use Par in replace of Node, fix some part

--- treewalk.cpp
+++ treewalk.cpp
@@ -5,54 +5,54 @@
 #include <algorithm>
 using namespace std;
 
-void ComputeMoments(Octree* tree, double* hmax, double* mass, double** quad, double* com)
+void ComputeMoments(double* mass, double* com, double** quad, double* hmax, Octree* tree)
 {
     // #####################
     // Note:
     // 1. quad is intitially 0
-    // 2. Some properties may be changed depending on how we deal with the node properties.
     // #####################
 
     bool IsParticle = true; // checking whether the current node is a particle
-    if ( tree->node == nullptr )   IsParticle = false;
+    if ( tree->par == nullptr )   IsParticle = false;
 
     double* hmaxi; // some properties for the child
     double* massi;
-    double* comi = new double[3]{0};
+    double*  comi  = new double[3]{0};
     double** quadi = new double*[3];
     for (int i = 0; i < 3; i++)  quadi[i] = new double[3]{0};
 
+    quad = quadi; // empty
+
     if ( IsParticle )
     {
-        hmax = tree->node->softening;
-        mass = tree->node->mass;
-        com = tree->node->pos;
-        quad = quadi; // empty
+        mass = tree->par->mass;
+        com = tree->par->pos;
+        hmax = tree->par->softening;
     }
     else // it is a node
     {
-        double hmax0 = 0; // some properties for the node
-        double m0 = 0; // total mass
+        double hmax0   = 0; // some properties for the node
+        double m0      = 0; // total mass
         double com0[3] = {}; // used to calculate COM of the node
 
         for (int octant=0; octant<8; octant++) // open the node to calculate the total mass and COM position
         {
-            ComputeMoments(tree->children[octant], hmaxi, massi, quadi, comi);
+            ComputeMoments(massi, comi, quadi, hmaxi, tree->children[octant]);
 
             hmax0 = max(hmax0, *hmaxi);
             m0 += *massi;
             for (int i = 0; i < 3; i++) com0[i] += (*massi)*comi[i];
         } // for (int octant=0; octant<8; octant++)
 
-        tree->Masses = mass = m0;
+        tree->Masses = mass = &m0;
         for (int i = 0; i < 3; i++) com[i] = com0[i]/m0;
 
         for (int octant=0; octant<8; octant++) // open the node to calculate quadrapoles from children
         {
             double* ri = new double[3];
-            double r2 = (double)0;
+            double  r2 = (double)0;
 
-            comi = tree->children[octant]->Coordinate;
+            comi  = tree->children[octant]->Coordinate;
             quadi = tree->children[octant]->Quadrapoles;
 
             for (int i = 0; i < 3; i++) ri[i] = comi[i] - com[i];
@@ -64,6 +64,8 @@ void ComputeMoments(Octree* tree, double* hmax, double* mass, double** quad, dou
                 quad[k][l] += quadi[k][l] + tree->children[octant]->Masses*3*ri[k]*ri[l];
                 if ( k==l ) quad[k][l] -= tree->children[octant]->Masses*r2;
             } // l, k
+
+            delete[] ri;
         } // for (int octant=0; octant<8; octant++)
 
         tree->Quadrapoles = quad;
@@ -75,9 +77,9 @@ void ComputeMoments(Octree* tree, double* hmax, double* mass, double** quad, dou
             delta += pow(dx, 2);
         } // for (int dim=0; dim<3; dim++)
 
-        tree->Deltas = sqrt(delta);
+        tree->Deltas      = sqrt(delta);
         tree->Coordinates = com;
-        tree->Softening = hmax = hmax0;
+        tree->Softening   = hmax = &hmax0;
     }
 
     for(int i = 0; i < 3; i++){
@@ -127,7 +129,7 @@ double PotentialWalk_quad(double* pos, Octree* tree, double theta, double soften
     double h = fmax(tree->Softenings, softening); // softening length
 
     bool IsParticle = true; // checking whether the current node is a particle
-    if ( tree->node == nullptr )   IsParticle = false;
+    if ( tree->par == nullptr )   IsParticle = false;
 
     if ( IsParticle ) // it is a particle
     {
