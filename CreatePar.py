1896e7ab98e0c02d6c83e8d04787bc330ac5edf8
myho@asiaa.sinica.edu.tw <myho@asiaa.sinica.edu.tw>
2023-05-26 15:34:58 +0800

    CreatePar.py: add reference result

--- CreatePar.py
+++ CreatePar.py
@@ -1,6 +1,7 @@
 # Usage: python CreatePar.py Npar
 import sys
 import numpy as np
+from pytreegrav import Potential
 
 N = int(sys.argv[1]) # number of particles
 pos = np.random.rand(N,3) # positions randomly sampled in the unit cube
@@ -14,5 +15,17 @@ for i in range(N):
         f.write("%13.7e %13.7e %13.7e %13.7e %13.7e" %(pos[i, 0], pos[i, 1], pos[i, 2], m[i], h[i]))
     else:
         f.write("%13.7e %13.7e %13.7e %13.7e %13.7e\n" %(pos[i, 0], pos[i, 1], pos[i, 2], m[i], h[i]))
+
+f.close()
+
+# Store reference potential
+p = Potential(pos,m,h)
+f = open("./Ref.dat", "w")
+
+for i in range(N):
+    if i==N-1:
+        f.write("%13.7e" %(p[i]))
+    else:
+        f.write("%13.7e\n" %(p[i]))
 
 f.close()
