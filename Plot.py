9536cd648e43946ad8261669933ad7088d95068c
Anchewei <10302leo@gmail.com>
2023-06-16 13:00:39 +0800

    Add moive.sh and modify Plot.py

--- Plot.py
+++ Plot.py
@@ -1,19 +1,20 @@
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 
+file_list = [i for i in os.listdir() if "Data_" in i]
 Origin      = np.loadtxt('Particle.dat', skiprows=1)
 Npar = Origin.shape[0]
 
-plt.figure(figsize=(8,6))
-plt.scatter(Origin[:, 0], Origin[:, 1], s=5)
-plt.xlim(0, 1)
-plt.ylim(0, 1)
-plt.savefig("00.png")
+if not os.path.exists('./Projection_Par'):
+    os.mkdir('./Projection_Par')
 
-for i in range(1, 11):
-    Timestep    = np.loadtxt('Timestep%02d.dat' % (i), skiprows=1)
-    plt.figure(figsize=(8,6))
-    plt.scatter(Timestep[:, 0], Timestep[:, 1], s=5)
-    plt.xlim(0, 1)
-    plt.ylim(0, 1)
-    plt.savefig('%02d.png' % (i))
+for data in file_list:
+    image_name = data.split('.')[0]+".png"
+    info    = np.loadtxt(data, skiprows=1)
+    fig = plt.figure(figsize=(8, 8))
+    ax = fig.add_subplot(111)
+    ax.scatter(info[:, 0], info[:, 1], s=5)
+    ax.set_xlim(0, 1)
+    ax.set_ylim(0, 1)
+    plt.savefig('./Projection_Par/'+image_name, dpi=150,bbox_inches='tight')
